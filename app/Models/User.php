bd8688df6c0739c6ed46d20e99838ff0b34a74ff
theoscar <oscardfer2020@gmail.com>
2026-05-12 11:45:56 -0500

    -Se actualiza el modelo User

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,8 +20,10 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'last_name',
         'email',
         'password',
+        'role',
     ];
 
     /**

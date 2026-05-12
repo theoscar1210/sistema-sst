f38f74289ee83a89e26377286ae78bf97b9b6c8b
theoscar <oscardfer2020@gmail.com>
2026-05-12 10:56:09 -0500

    se crea la tabla cursos

--- database/migrations/2026_05_12_155010_create_courses_table.php
+++ database/migrations/2026_05_12_155010_create_courses_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 150);
+            $table->integer('validity_months');
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
